fc944080111de63d9650ef784228eb13ac1178d7
ItsYaBoiBry <bryanlow987@gmail.com>
2017-05-25 11:52:47 +0800

    added notifications for btnTwo

--- app/src/main/java/android/myapplicationdev/com/demonotification/MainActivity.java
+++ app/src/main/java/android/myapplicationdev/com/demonotification/MainActivity.java
@@ -49,6 +49,40 @@ public class MainActivity extends AppCompatActivity {
 
                 notificationManager.notify(notificationID, n);
                 finish();
+            }
+        });
+        btn_notify_two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+
+                PendingIntent pendingIntent = PendingIntent.getActivity
+                        (MainActivity.this, requestCode,
+                                intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                Notification.BigTextStyle bigText = new
+                        Notification.BigTextStyle();
+                bigText.bigText("This is one big text");
+                bigText.setBigContentTitle("Big Text – Long Content");
+                bigText.setSummaryText("Reflection Journal?");
+                // Build notification
+                Notification.Builder builder = new
+                        Notification.Builder(MainActivity.this);
+                builder.setContentTitle("Amazing Offer!");
+                builder.setContentText("Subject");
+                builder.setSmallIcon(R.mipmap.ic_launcher);
+                builder.setContentIntent(pendingIntent);
+                builder.setStyle(bigText);
+                builder.setAutoCancel(true);
+
+                Notification n = builder.build();
+
+                NotificationManager notificationManager = (NotificationManager)
+                        getSystemService(NOTIFICATION_SERVICE);
+
+                // This replaces the existing notification with the same ID
+                notificationManager.notify(notificationID, n);
+                finish();
 
 
             }
